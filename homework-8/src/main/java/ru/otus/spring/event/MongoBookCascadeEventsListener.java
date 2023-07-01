a10fdbabbaff0d64024a0bab8438aaede539e4b6
Nick <67371365+niporka1992@users.noreply.github.com>
2023-07-01 17:50:42 +0300

    refactor

--- homework-8/src/main/java/ru/otus/spring/event/MongoBookCascadeEventsListener.java
+++ homework-8/src/main/java/ru/otus/spring/event/MongoBookCascadeEventsListener.java
@@ -5,10 +5,7 @@ import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventLis
 import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
 import org.springframework.stereotype.Component;
 import ru.otus.spring.entities.Book;
-import ru.otus.spring.entities.Comment;
 import ru.otus.spring.repositories.CommentRepository;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -20,9 +17,9 @@ public class MongoBookCascadeEventsListener extends AbstractMongoEventListener<B
     public void onAfterDelete(AfterDeleteEvent<Book> event) {
         super.onAfterDelete(event);
         var bookId = event.getSource().get("_id").toString();
-        List<Comment> comments = commentRepository.findByBookId(bookId);
-        if (!comments.isEmpty()) {
-            commentRepository.deleteAll(comments);
+        Book book = new Book(bookId);
+        if (commentRepository.countCommentByBook(book) < 1) {
+            commentRepository.deleteAllByBook(book);
         }
     }
 }
