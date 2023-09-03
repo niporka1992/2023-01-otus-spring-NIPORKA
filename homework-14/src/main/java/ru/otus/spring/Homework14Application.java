dba062eb4b0c532518f5845b27f878301369adc5
Nick <67371365+niporka1992@users.noreply.github.com>
2023-09-04 02:07:33 +0300

    refactor

--- homework-14/src/main/java/ru/otus/spring/Homework14Application.java
+++ homework-14/src/main/java/ru/otus/spring/Homework14Application.java
@@ -1,24 +1,15 @@
 package ru.otus.spring;
 
 import com.github.cloudyrock.spring.v5.EnableMongock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
 
 @SpringBootApplication
 @EnableMongock
 public class Homework14Application {
-    @Autowired
-    private static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         System.out.println("http://localhost:8080/");
         SpringApplication.run(Homework14Application.class, args);
-
-
-// Получение бина по имени
-        var springShellBean = applicationContext.getBean("springShellBeanName");
-        System.out.println(springShellBean);
     }
 }
