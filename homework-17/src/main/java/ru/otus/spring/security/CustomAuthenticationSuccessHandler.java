45011edfd1a2363645abe9c876636dce6d9d2e7b
Nick <67371365+niporka1992@users.noreply.github.com>
2023-10-11 19:07:22 +0300

    docker (#21)
    * docker
    
    * refactor
    
    * refactor

---
+++ homework-17/src/main/java/ru/otus/spring/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,32 @@
+package ru.otus.spring.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        List<String> roles = new ArrayList<>();
+        for (GrantedAuthority authority : authorities) {
+            roles.add(authority.getAuthority());
+        }
+
+        // Устанавливаем роли в сессию или как атрибут запроса
+        request.getSession().setAttribute("currentUserRoles", roles);
+
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
